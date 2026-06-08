a2a1e532423f7399617e6cbd64891cbc010245bc
martinZonis <49437822@est.ort.edu.ar>
2026-06-08 08:03:12 -0300

    ,

---
+++ appLaberinto/app_laberinto/README.md
@@ -0,0 +1,3 @@
+# app_laberinto
+
+A new Flutter project.
---
+++ appLaberinto/app_laberinto/android/app/src/main/kotlin/com/example/app_laberinto/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_laberinto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
