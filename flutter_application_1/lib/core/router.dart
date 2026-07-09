a3492ab36fd0e5547965c7fb3789ca9ae1e6dba2
martinZonis <49437822@est.ort.edu.ar>
2026-07-09 19:31:48 -0300

    Update router.dart

--- flutter_application_1/lib/core/router.dart
+++ flutter_application_1/lib/core/router.dart
@@ -1,20 +1,20 @@
 import 'package:go_router/go_router.dart';
 import 'package:flutter_application_1/pantallas/home_page.dart';
 import 'package:flutter_application_1/pantallas/login.dart';
-import 'package:flutter_application_1/pantallas/result_page.dart';
+//import 'package:flutter_application_1/pantallas/result_page.dart';
 import 'package:flutter_application_1/classes/product.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/login',
   routes: [
-    GoRoute(path: '/login', builder: (context, state) => const Login()),
-    GoRoute(path: '/home', builder: (context, state) =>  HomePage()),
     GoRoute(
-      path: '/result', 
-      builder: (context, state) {
-        final productsList = state.extra as List<Product>? ?? [];
-        return ResultPage(products: productsList);
-      }
+      path: '/login', 
+      builder: (context, state) => const Login(),
     ),
-  ]
+    GoRoute(
+      path: '/home', 
+      builder: (context, state) => HomePage(),
+    ),
+    // Todo el bloque de GoRoute de '/result'
+  ],
 );
